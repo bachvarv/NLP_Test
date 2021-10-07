474f7e328102f56e970d20c02388f689d4110c00
Vladislav Bachvarov <v.bachvarov@web.de>
2021-10-07 21:46:31 +0200

    First commit

--- bert/fine_tuning/main_fine_tuning.py
+++ bert/fine_tuning/main_fine_tuning.py
@@ -1,9 +1,13 @@
 from keras.layers import TextVectorization
 
+import platform
+import os
 from bert.fine_tuning.data.BERTFineTuningModel import BERTFineTuningModel
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
+from official.nlp import optimization, bert
+import official.nlp.bert.tokenization
 import tensorflow_hub as hub
 
 def pre_process_input(input, batch_size, max_sentence_size):
@@ -16,7 +20,8 @@ def pre_process_input(input, batch_size, max_sentence_size):
         input_mask[:array_size, :array.shape[1], ] = np.ones(array.shape)
         input_type_ids = np.zeros((batch_size, max_sentence_size)).astype(int)
     else:
-        array = input.numpy()
+        # array = input.numpy()
+        array = input
         array_size = len(array)
         input_mask = np.zeros((array_size, max_sentence_size)).astype(int)
         processed_input = np.zeros((array_size, max_sentence_size)).astype(int)
@@ -26,13 +31,20 @@ def pre_process_input(input, batch_size, max_sentence_size):
                 processed_input[i][:len(array[i]), ] = array[i]
                 input_mask[i][:len(array[i]), ] = np.ones(array[i].shape)
             else:
-                return dict()
+                return None, None, None
 
-    return {'input_mask': input_mask,
-            'input_type_ids': input_type_ids,
-            'input_word_ids': processed_input}
+    return input_mask, input_type_ids, processed_input
 
 
+def encode_sentence(s):
+
+    tokenized_sentences = list(act_tokenizer.tokenize(s.numpy()[0]))
+    tokenized_sentences.insert(0, '[CLS]')
+    tokenized_sentences.append('[SEP]')
+    ids = act_tokenizer.convert_tokens_to_ids(tokenized_sentences)
+    return ids
+
+#TODO: make a better preprocessing of the dataset
 pre_trained_model = 'https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/4'
 sequence_size = 128
 batch_size = 2
@@ -40,17 +52,18 @@ batch_size = 2
 examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
                                with_info=True, as_supervised=True)
 
-train_examples, val_examples = examples['train'], examples['validation']
-en_examples = []
-pt_examples = []
 
-model_name = 'ted_hrlr_translate_pt_en_converter'
-tf.keras.utils.get_file(f"{model_name}.zip",
-                                    f"https://storage.googleapis.com/download.tensorflow.org/models/{model_name}.zip",
-                                    cache_dir='.', cache_subdir='', extract=True)
-tokenizer = tf.saved_model.load(model_name)
-en_tokenizer = tokenizer.en
-pt_tokenizer = tokenizer.pt
+train_examples, val_examples = examples['train'], examples['validation']
+
+if(platform.system() == 'Linux'):
+    model_checkpoint = 'checkpoint/bert_multi_cased_L-12_H-768_A-12_4'
+    vocab_path = os.path.join(model_checkpoint, "assets/vocab.txt")
+else:
+    model_checkpoint = 'E:\\Masterarbeit\\pre-trained bert\\bert_multi_cased_L-12_H-768_A-12_4'
+    vocab_path = os.path.join(model_checkpoint, "assets\\vocab.txt")
+
+# TODO: new Tokenizer remove the old one
+act_tokenizer = bert.tokenization.FullTokenizer(vocab_file=vocab_path, do_lower_case=True)
 
 
 # train_pt_examples, train_en_examples = train_examples.batch(30000).take(5)
@@ -59,36 +72,141 @@ pt_tokenizer = tokenizer.pt
 # print(train_en_examples[0][0])
 
 # preprocessor = hub.load("https://tfhub.dev/tensorflow/bert_multi_cased_preprocess/3")
-train_ex = []
-one_sent = ''
+
+cls = act_tokenizer.convert_tokens_to_ids(['[CLS]'])
+train_inp_ex = []
+train_label_ex = []
 over = False
-for train_pt_examples, train_en_examples in train_examples.batch(batch_size):
+for train_pt_examples, train_en_examples in train_examples.batch(1):
+    # train_en_examples = train_en_examples.numpy()
+    # train_pt_examples = train_pt_examples.numpy()
     for example in train_pt_examples:
-        one_sent = len(example.numpy().split())
-        if one_sent > sequence_size:
+
+        one_sent_pt = len(example.numpy())
+        if one_sent_pt > sequence_size:
             over = True
             break
-    if over is False:
-        for example in train_en_examples:
-            one_sent = len(example.numpy().split())
-            if one_sent > sequence_size:
-                over = True
-                break
+    for example in train_en_examples:
+        one_sent_en = len(example.numpy())
+        if one_sent_en > sequence_size:
+            over = True
+            break
 
-    if over is True:
+    if over:
         over = False
         continue
+    # TODO: implement the act_tokenizer
+    train_en_example = encode_sentence(train_en_examples)
+    train_pt_example = encode_sentence(train_pt_examples)
+    train_inp_ex.append(train_en_example)
+    train_label_ex.append(train_pt_example)
+    # train_en_examples = act_tokenizer.tokenize(train_en_examples.numpy())
+    # train_pt_examples = act_tokenizer.tokenize(train_pt_examples.numpy())
+    # train_en_examples = act_tokenizer.convert_tokens_to_ids(train_en_examples)
+    # train_pt_examples = act_tokenizer.convert_tokens_to_ids(train_pt_examples)
+    # input_mask, input_type_ids, input_word_ids = pre_process_input(train_en_example, batch_size, sequence_size)
 
-    train_en_examples = en_tokenizer.tokenize(train_en_examples)
-    train_pt_examples = pt_tokenizer.tokenize(train_pt_examples)
 
-    en_ex = pre_process_input(train_en_examples, batch_size, sequence_size)
-    pt_ex = pre_process_input(train_pt_examples, batch_size, sequence_size)
-    train_ex.append((en_ex, pt_ex))
+inp_data = []
+lab_data = []
+input_word_ids = []
+input_type_ids = []
+input_mask = []
+for i in range(len(train_inp_ex)):
+    inp_length = len(train_inp_ex[i])
+    lab_length = len(train_label_ex[i])
+    if inp_length > sequence_size or lab_length > sequence_size:
+        continue
 
+    input_word_ids.append(np.concatenate(
+        (train_inp_ex[i], np.zeros(sequence_size - inp_length, dtype='int'))
+    ))
+    input_type_ids.append(np.zeros(sequence_size, dtype='int'))
+    input_mask.append(np.concatenate(
+        (np.ones_like(train_inp_ex[i]), np.zeros(sequence_size - inp_length, dtype='int'))
+    ))
 
-# print(train_ex[0])
-vocab_size = pt_tokenizer.get_vocab_size()
+    lab_data.append(np.concatenate(
+        (train_label_ex[i], np.zeros(sequence_size - inp_length, dtype='int'))
+    ))
+
+lab_data = tf.ragged.constant(lab_data).to_tensor()
+input_mask = tf.ragged.constant(input_mask).to_tensor()
+input_word_ids = tf.ragged.constant(input_mask).to_tensor()
+input_type_ids = tf.ragged.constant(input_type_ids).to_tensor()
+
+print(f'Input shape: {input_word_ids.shape}')
+print(f'Label shape: {lab_data.shape}')
+
+inp_data = dict(
+    input_word_ids=input_word_ids,
+    input_type_ids=input_type_ids,
+    input_mask=input_mask
+)
+
+data = (inp_data, lab_data)
+
+# This way doesn't work
+# en_examples = []
+# pt_examples = []
+#
+# for label, input in train_examples.batch(1):
+#     label = list(act_tokenizer.tokenize(label.numpy()[0]))
+#     input = list(act_tokenizer.tokenize(input.numpy()[0]))
+#     one_sent = len(label)
+#     if one_sent > sequence_size:
+#         break
+#
+#     one_sent = len(input)
+#     if one_sent > sequence_size:
+#         break
+#     label.insert( 0, '[CLS]')
+#     label.append('[SEP]')
+#     input.insert(0, '[CLS]')
+#     input.append('[SEP]')
+#     print(input)
+#     print(label)
+#
+#     label = act_tokenizer.convert_tokens_to_ids(label)
+#     input = act_tokenizer.convert_tokens_to_ids(input)
+#
+#     en_examples.append(input)
+#     pt_examples.append(label)
+#
+# input_word_ids = []
+# input_mask = []
+# input_type_ids = []
+# labels = []
+# for i in range(len(en_examples)):
+#     en_length = len(en_examples[i])
+#     pt_length = len(pt_examples[i])
+#     if en_length > sequence_size or pt_length > sequence_size:
+#         continue
+#
+#     input_word_ids.append(np.concatenate(
+#         (en_examples[i], np.zeros((sequence_size - en_length), dtype='int'))
+#     ))
+#     input_mask.append(np.concatenate(
+#         (np.ones(en_length, dtype='int'), np.zeros((sequence_size - en_length), dtype='int'))
+#     ))
+#     input_type_ids.append(np.zeros(sequence_size, dtype='int'))
+#
+#     labels.append(np.concatenate(
+#         (pt_examples[i], np.zeros((sequence_size - pt_length), dtype='int'))
+#     ))
+#
+# labels = tf.ragged.constant(labels).to_tensor()
+# input_word_ids = tf.ragged.constant(input_word_ids)
+# input_type_ids = tf.ragged.constant(input_type_ids)
+# input_mask = tf.ragged.constant(input_mask)
+#
+vocab_size = len(act_tokenizer.vocab)
+#
+# input_ds = dict(
+#     input_word_ids=input_word_ids.to_tensor(),
+#     input_type_ids=input_type_ids.to_tensor(),
+#     input_mask=input_mask.to_tensor()
+# )
 
 # print(vocab_size)
 
@@ -105,12 +223,7 @@ bert = BERTFineTuningModel(pre_trained_model, batch_size, sequence_size, vocab_s
 bert.compile(optimizer='adam',
              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
 
-
-tf.keras.utils.plot_model(bert, show_shapes=True, dpi=48, to_file='model_1.png')
-
-train_example_size = len(en_examples)
-bert.train(train_ex, train_example_size, 1)
-
+bert.train(data, 1)
 
 
 # Use sparse categorical crossentropy when your classes are mutually exclusive
--- bert/test.py
+++ bert/test.py
@@ -1,20 +1,33 @@
+import os
 import platform
+from io import BytesIO
+from zipfile import ZipFile
+from urllib.request import urlopen
+
 import tensorflow as tf
+import requests
+from os.path import exists
 
 from bert.corpus_utils.utils import download_data_wiki, _tokenize
 from bert.data.BERTModel import BERTModel
 
-if(platform.system() == 'Linux'):
-    dir = '/home/bachvarv/Abschlussarbeit/Corpus/wikitext-2-v1/wikitext-2/'
-else:
-    dir = 'E:\Masterarbeit\Corpus\wikitext-2-v1\wikitext-2'
-
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
+if(platform.system() == 'Linux'):
+    dir = os.curdir + "/data/corpus/wikitext-2/"
+else:
+    dir = 'E:\Masterarbeit\Corpus\wikitext-2'
+
+if not exists(os.curdir + '/data/corpus/wikitext-2/'):
+    path = os.curdir + '/data/corpus/'
+    url = 'https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-v1.zip'
+    corpus = urlopen(url)
+    zip_file = ZipFile(BytesIO(corpus.read()))
+    zip_file.extractall(path)
 # paragraphs = _read_text(dir)
 
 cfg = {
-    'batch_size': 64,
+    'batch_size': 16,
     'input_max_len': 64,
     'num_layers': 12,
     'd_model': 768,
@@ -40,14 +53,16 @@ bert.compile(optimizer='adam',
 
 iterator = dataset_iter.as_numpy_iterator()
 
+
+
 tokens_ids, segments, valid_lens, pred_positions, mlm_weights, mlm_labels, nsp_labels = iterator.next()
 
 bert(tokens_ids, segments, pred_positions)
-tf.keras.utils.plot_model(bert)
-# bert.train_step(iterator, 1)
+# tf.keras.utils.plot_model(bert)
+bert.train_step(iterator, 1)
 
 if(platform.system() == 'Linux'):
-    checkpoint_path = ''
+    checkpoint_path = 'checkpoint/training/'
 else:
     checkpoint_path ="E:\\Masterarbeit\\pre-trained bert\\bert_multi_cased_L-12_H-768_A-12_4.tar\\variables\\variables.index"
 
