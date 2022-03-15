ec3282109f593da1b48d30247a8a5ac2c97d0768
bachvarv <v.bachvarov>
2022-03-15 17:32:09 +0100

    Neural Machine Translation Model

---
+++ simple_language/nmt_model2_test.py
@@ -0,0 +1,262 @@
+import os
+import random
+import subprocess
+import transformers
+import tensorflow as tf
+from simple_language.transformer_data.NMTModel import NMTModel
+from simple_language.transformer_data.NMTModel_BERT_Encoding import NMTModel_BERT_Encoding
+
+
+def git(*args):
+    return subprocess.check_call(['git'] + list(args))
+
+
+def save_history(history, file):
+
+    with open(file, 'a+') as f:
+        for i in range(len(history.history['accuracy'])):
+            line = f"{history.history['accuracy'][i]}\t{history.history['loss'][i]}\n"
+            f.writelines(line)
+        f.close()
+
+model_name = os.path.join(os.curdir, 'bert-base-german-cased')
+path_to_history = os.path.join(os.curdir, 'history_nmt_mix_v1.csv')
+
+if not os.path.isdir(model_name):
+    url = 'https://huggingface.co/bert-base-german-cased'
+    git("clone", url)
+
+# The Tokenizer
+tokenizer = transformers.BertTokenizer.from_pretrained(model_name)
+print(tokenizer.add_tokens(['[GO]'], True))
+# tokenizer.add_special_tokens(['[GO]'])
+vocab_size = tokenizer.vocab_size + 1
+print(vocab_size)
+# config = tf.compat.v1.ConfigProto(gpu_options=tf.compat.v1.GPUOptions(per_process_gpu_memory_fraction=0.8))
+# config.gpu_options.allow_growth = True
+# session = tf.compat.v1.Session(config=config)
+# tf.compat.v1.keras.backend.set_session(session)
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+if tf.test.gpu_device_name():
+    print('GPU found')
+else:
+    print("No GPU found")
+
+# Config variables
+cfg = dict(
+    max_sentence=128,
+    hidden_layer_size=768,
+    batch_size=1,
+    transformer_heads=12,
+    head_size=64
+)
+
+# Corpus preparations
+input_ids = []
+token_type_ids = []
+attention_masks = []
+labels = []
+
+eval_ids = []
+eval_type_ids = []
+eval_masks = []
+eval_labels = []
+
+input_arr = []
+eval_arr = []
+label_arr = []
+eval_label_arr = []
+path_to_corpus = os.path.join(os.path.join(os.curdir, 'corpus'), 'einfache_sprache.csv')
+path_to_corpus_2 = os.path.join(os.path.join(os.curdir, 'corpus'), 'spd_programm_einfache_sprache_v1.csv')
+path_to_corpus_3 = os.path.join(os.path.join(os.curdir, 'corpus'), 'simple_language_openAI.csv')
+longest_cand = 0
+with open(path_to_corpus, 'r', encoding='utf-8') as file:
+
+    lines = file.readlines()
+    # print(len(lines))
+    for line in lines:
+
+        x, y = line.split(sep='\t')
+        # tokenized_x = tokenizer.tokenize(x)
+        # tokenized_y = tokenizer.tokenize(y)
+        # token_x = tokenizer(x, return_tensors='tf')
+        # token_y = tokenizer(y, return_tensors='tf')
+        # input_arr.append(token_x)
+        # label_arr.append(token_y)
+    # if 12 < len(tokenized_y) <= 30:
+    #     print(len(tokenized_y))
+        if random.randint(0, 100) > 10:
+            input_arr.append(x)
+            # label_arr.append('[CLS]' + y) # nmt_model_v2
+            label_arr.append('[GO]' + y) # nmt_model_v4
+            # label_arr.append(y)
+            # input_arr.append(tokenized_x)
+            # label_arr.append(tokenized_y)
+
+        else:
+            eval_arr.append(x)
+            # eval_label_arr.append('[CLS]' + y) # nmt_model_v2
+            eval_label_arr.append('[GO]' + y) # nmt_model_v4
+            # eval_label_arr.append(y)
+            # eval_arr.append(tokenized_x)
+            # eval_label_arr.append(tokenized_y)
+
+        # size_x = token_x['input_ids'].shape[1]
+        # size_y = token_y['input_ids'].shape[1]
+        # if size_x > size_y:
+        #     if size_x > longest_cand:
+        #         longest_cand = size_x
+        # else:
+        #     if size_y > longest_cand:
+        #         longest_cand = size_y
+
+
+with open(path_to_corpus_2, 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+    for line in lines:
+        x, y = line.split(sep='\t')
+        # tokenized_x = tokenizer.tokenize(x)
+        # tokenized_y = tokenizer.tokenize(y)
+    # if 12 < len(tokenized_y) <= 30:
+    #     print(len(tokenized_y))
+        if random.randint(0, 100) > 10:
+            input_arr.append(x)
+            # label_arr.append('[CLS]' + y) # nmt_model_v2
+            label_arr.append('[GO]' + y) # nmt_model_v4
+            # label_arr.append(y)
+            # input_arr.append(tokenized_x)
+            # label_arr.append(tokenized_y)
+
+        else:
+            eval_arr.append(x)
+            # eval_label_arr.append('[CLS]' + y) # nmt_model_v2
+            eval_label_arr.append('[GO]' + y) # nmt_model_v4
+            # eval_label_arr.append(y)
+            # eval_arr.append(tokenized_x)
+            # eval_label_arr.append(tokenized_y)
+
+with open(path_to_corpus_3, 'r', encoding='utf-8') as file:
+    lines = file.readlines()
+    for line in lines:
+        x, y = line.split(sep='\t')
+        # tokenized_x = tokenizer.tokenize(x)
+        # tokenized_y = tokenizer.tokenize(y)
+    # if 12 < len(tokenized_y) <= 30:
+    #     print(len(tokenized_y))
+        if random.randint(0, 100) > 10:
+            input_arr.append(x)
+            # label_arr.append(y)
+            # label_arr.append('[CLS]' + y) # nmt_model_v2
+            label_arr.append('[GO]' + y) # nmt_model_v4
+            # input_arr.append(tokenized_x)
+            # label_arr.append(tokenized_y)
+
+        else:
+            eval_arr.append(x)
+
+            # eval_label_arr.append('[CLS]' + y) # nmt_model_v2
+            eval_label_arr.append('[GO]' + y) # nmt_model_v2
+            # eval_label_arr.append(y)
+
+# creating the dataset
+
+#
+# def tokenize_function(examples):
+#     return tokenizer(examples, max_length=128, return_tensors='tf')
+
+
+# arr = np.reshape(input_arr, newshape=[1, len(input_arr)])
+arr_inp = tokenizer(input_arr, max_length=cfg['max_sentence'], padding='max_length', truncation=True, return_tensors='tf')
+arr_lab = tokenizer(label_arr, max_length=cfg['max_sentence'], padding='max_length', truncation=True, return_tensors='tf')
+# print(arr_inp)
+
+arr_eval = tokenizer(eval_arr, max_length=cfg['max_sentence'], padding='max_length', truncation=True, return_tensors='tf')
+arr_eval_label = tokenizer(eval_label_arr, max_length=cfg['max_sentence'], padding='max_length', truncation=True, return_tensors='tf')
+# print(arr_eval)
+### Old Way
+# print(non_tokenized)
+
+dataset = tf.data.Dataset.from_tensor_slices((
+    dict(input_ids=arr_inp['input_ids'],
+         token_type_ids=arr_inp['token_type_ids'],
+         attention_mask=arr_inp['attention_mask']),
+    dict(input_ids=arr_lab['input_ids'],
+         token_type_ids=arr_lab['token_type_ids'],
+         attention_mask=arr_lab['attention_mask']))).batch(1)
+
+eval_dataset = tf.data.Dataset.from_tensor_slices((
+    dict(input_ids=arr_eval['input_ids'],
+         token_type_ids=arr_eval['token_type_ids'],
+         attention_mask=arr_eval['attention_mask']),
+    dict(input_ids=arr_eval_label['input_ids'],
+         token_type_ids=arr_eval_label['token_type_ids'],
+         attention_mask=arr_eval_label['attention_mask']))).batch(1)
+
+model = NMTModel_BERT_Encoding(model_name, cfg['hidden_layer_size'], cfg['transformer_heads'], cfg['max_sentence'], vocab_size)
+model.embedding_layer.resize_token_embeddings(len(tokenizer))
+model.compile(optimizer=model.optimizer,
+              loss=model.loss,
+              metrics=['accuracy'])
+
+# Checkpoint
+path_to_checkpoint = os.path.join(os.curdir, 'model_nmt_mix_v5')
+# path_to_saved_model = os.path.join(os.curdir, 'saved_model_gru_1024_v3')
+
+ckpt = tf.train.Checkpoint(model)
+ckpt_manager = tf.train.CheckpointManager(ckpt, path_to_checkpoint, max_to_keep=1)
+
+if ckpt_manager.latest_checkpoint:
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+    print(f'Loaded checkpoint from {ckpt_manager.latest_checkpoint}')
+else:
+    print('Initializing from scratch!')
+
+
+test_input = tokenizer('Anrede', max_length=cfg['max_sentence'], padding='max_length', return_tensors='tf')
+test_target = tokenizer('[GO]', max_length=cfg['max_sentence'], padding='max_length', return_tensors='tf')
+
+inputs = (dict(input_ids=test_input['input_ids'],
+              token_type_ids=test_input['token_type_ids'],
+              attention_mask=test_input['attention_mask']),
+          dict(input_ids=test_target['input_ids'],
+               token_type_ids=test_target['token_type_ids'],
+               attention_mask=test_target['attention_mask'])
+          )
+
+output = model(inputs, False)
+print(output.shape)
+print(output)
+output = tf.argmax(output, axis=-1)
+print(output)
+print(tokenizer.decode(output[0]))
+
+model.summary()
+
+
+
+# history = model.fit(dataset, epochs=5)
+# model.evaluate(eval_dataset)
+model.train_step(dataset, epochs=2)
+ckpt_manager.save()
+
+sentence = ['Anrede']
+pred = ['[GO]']
+token = tokenizer(sentence, max_length=cfg['max_sentence'], padding='max_length', return_tensors='tf')
+pred_token = tokenizer(pred, max_length=cfg['max_sentence'], padding='max_length', return_tensors='tf')
+test_inp = (dict(input_ids=token['input_ids'],
+              token_type_ids=token['token_type_ids'],
+              attention_mask=token['attention_mask']),
+            dict(input_ids=pred_token['input_ids'],
+              token_type_ids=pred_token['token_type_ids'],
+              attention_mask=pred_token['attention_mask']))
+predicted = model(test_inp, False)
+print(predicted.shape)
+arg_max = tf.argmax(predicted, axis=-1)
+print(arg_max)
+print(tokenizer.decode(arg_max[0]))
+
+# save_history(history, path_to_history)
+
+
---
+++ simple_language/transformer_data/NMTModel_BERT_Encoding.py
@@ -0,0 +1,105 @@
+import time
+
+import tensorflow as tf
+import transformers
+
+from simple_language.transformer_data.NMTDecoderLayer import NMTDecoderLayer
+from simple_language.transformer_data.NMTEncoderLayer import NMTEncoderLayer
+from tensorflow.keras.layers import Embedding
+
+def create_padding_mask(seq):
+    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+
+    # add extra dimensions to add the padding
+    # to the attention logits.
+    return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
+
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask  # (seq_len, seq_len)
+
+
+class NMTModel_BERT_Encoding(tf.keras.Model):
+    def __init__(self, model_name, hidden_layer_size, transformer_heads, seq_len, vocab_size):
+        super(NMTModel_BERT_Encoding, self).__init__()
+        self.heads = transformer_heads
+        self.embedding_layer = transformers.TFBertModel.from_pretrained(model_name)
+        self.encoder_layers = [NMTEncoderLayer(hidden_layer_size,
+                                               transformer_heads,
+                                               1e-3) for _ in range(transformer_heads)]
+
+        self.decoder_emb = Embedding(vocab_size, hidden_layer_size, input_length=seq_len)
+        self.decoder_layers = [NMTDecoderLayer(hidden_layer_size,
+                                               transformer_heads,
+                                               1e-3) for _ in range(transformer_heads)]
+
+        self.dense_layer = tf.keras.layers.Dense(vocab_size)
+
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+        self.loss = tf.keras.losses.SparseCategoricalCrossentropy()
+
+    def call(self, inputs, training):
+        inp, target = inputs
+        # print(target)
+        # print(enc_self_att)
+        # print(inp['attention_mask'])
+        enc_padding_mask, dec_padding_mask, look_ahead_mask = self.create_masks(inp['input_ids'], target['input_ids'])
+
+        encoding_out = self.embedding_layer(inp).last_hidden_state
+        dec_encoding_out = self.embedding_layer(target).last_hidden_state
+        # print(enc_out.shape)
+        enc_self_att = self.encoder_layers[0](encoding_out, encoding_out, training, enc_padding_mask)
+        for i in range(1, self.heads):
+            enc_self_att = self.encoder_layers[i](enc_self_att, encoding_out, training, enc_padding_mask)
+
+        # target_enc = self.decoder_emb(target)
+        target_enc = self.decoder_layers[0](dec_encoding_out, encoding_out, enc_self_att, training, look_ahead_mask, dec_padding_mask)
+        for i in range(self.heads):
+            target_enc = self.decoder_layers[i](target_enc, encoding_out, enc_self_att, training, look_ahead_mask, dec_padding_mask)
+
+        output = self.dense_layer(target_enc)
+
+        return output
+    def create_masks(self, inp, tar):
+        enc_padding_mask = create_padding_mask(inp)
+
+        dec_padding_mask = create_padding_mask(inp)
+
+        # Used in the 1st attention block in the decoder.
+        # It is used to pad and mask future tokens in the input received by
+        # the decoder.
+        look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
+        dec_target_padding_mask = create_padding_mask(tar)
+        look_ahead_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+
+        return enc_padding_mask, dec_padding_mask, look_ahead_mask
+
+    def train_step(self, dataset, epochs=5):
+        start = time.time()
+        for i in range(1, epochs+1):
+            epoch_start = time.time()
+            for step, element in enumerate(dataset):
+                _, target = element
+                with tf.GradientTape() as tape:
+                    prediction = self.call(element, True)
+
+                    loss = self.loss(target['input_ids'], prediction)
+
+                grads = tape.gradient(loss, self.trainable_weights)
+
+                self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+
+                # Log every 200 batches.
+                if step % 20 == 0:
+                    print(
+                        "Training loss (for one batch) at step %d: %.4f"
+                        % (step, float(loss))
+                    )
+                    print("Seen so far: %s samples" % ((step + 1) * 1))
+            end = time.time()
+            print('Epoch #%d' % i)
+            print('Execution time was %d s' % (end - epoch_start))
+        print('Execution time was %d s' % (end - start))
+
+
